7c1427b189a50a26f4573d4a7943f2a4a97d1d56
extendslcc <lucas.lcc@hotmail.com>
2020-06-30 22:23:07 -0300

    :heavy_plus_sign: ADD DEPENDENCY: added sweetalert2

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -41,3 +41,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+// sweet alert2
+window.swal = require( 'sweetalert2' )
