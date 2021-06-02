7ba0070026c840b96d992ac8fde466019405c6ef
extendslcc <lucas.lcc@hotmail.com>
2021-06-02 00:18:02 -0300

    :construction: WIP: created a home page route and view

---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,25 @@
+@extends( 'layouts.default' )
+
+
+@section( 'title', 'Home' )
+
+@section( 'content' )
+   
+   {{-- <div class="container">
+        <!--Section: Content-->
+        <section>
+            <!-- Jumbotron -->
+            <div id="intro" class="p-5 text-center bg-light shadow-5 rounded mb-5">
+                <h1 class="mb-3 h2">Learn Bootstrap 5 with MDB</h1>
+                <p class="mb-3">Best & free guide of responsive web design</p>
+                <a class="btn btn-primary m-2" href="https://www.youtube.com/watch?v=c9B4TPnak1A" role="button" rel="nofollow"
+                   target="_blank">Start tutorial</a>
+                <a class="btn btn-primary m-2" href="https://mdbootstrap.com/docs/standard/" target="_blank"
+                   role="button">Download MDB UI KIT</a>
+            </div>
+            <!-- Jumbotron -->
+        </section>
+        <!--Section: Content-->
+    </div>--}}
+
+@endsection
