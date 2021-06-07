588e7083ba4ea6006dff65278f5bbdef7b46599e
extendslcc <lucas.lcc@hotmail.com>
2021-06-06 23:10:14 -0300

    :construction: WIP: working on an automated dynamic bundling for assets files in view folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
 const mix = require( 'laravel-mix' );
+const MixGlob = require( 'laravel-mix-glob' );
+const del = require( 'del' );
+const env = process.env.NODE_ENV || 'dev';
 
 /*
  |--------------------------------------------------------------------------
@@ -11,14 +14,45 @@ const mix = require( 'laravel-mix' );
  |
  */
 
+
+/**
+ * How assets should be handled
+ *
+ * app.scss | views/components/*.scss | views/sections/*.scss -> public/app.css
+ *
+ * views/pages/**|*.scss -> public/pages/[page-name].css
+ *
+ * -> public/vendor.css
+ */
+
+const mixGlob = new MixGlob( { mix } );
+
+mixGlob
+    .js( 'resources/views/{sections,components}/**/*.js', 'public/js/combine' )
+    .sass( 'resources/views/{sections,components}/**/*.scss', 'public/css/combine' );
+
 mix
     .browserSync( 'localhost:8000' )
     .disableNotifications()
-    .extract()
+    .extract( 'public/js/vendor.js' )
     .js( 'resources/js/app.js', 'public/js' )
     .postCss( 'resources/css/app.css', 'public/css' )
     .postCss( 'resources/css/vendor.css', 'public/css', [
         require( 'tailwindcss' ),
     ] )
-    .sass( 'resources/sass/app.scss', 'public/css' )
-    .sass( 'resources/sass/vendor.scss', 'public/css' );
+    .sass( 'resources/sass/app.scss', 'public/css/combine' )
+    .sass( 'resources/sass/vendor.scss', 'public/css' )
+    // combine app.css with {components,sections}/styles.css
+    .styles( 'public/css/combine/*', 'public/css/app.css' )
+    // combine app.js with with {components,sections}/script.index
+    .scripts( ['public/js/app.js', 'public/js/combine/*'], 'public/js/app.js' )
+    .then( () => {
+
+        if (env === 'production') {
+
+            del( 'public/js/combine' );
+            del( 'public/css/combine' );
+
+        }
+
+    } );
