1d0d5da8118db0ed5cb854d9acb7c1507b753b34
extendslcc <lucas.lcc@hotmail.com>
2021-06-07 00:26:50 -0300

    :recycle: REFACTOR: improved automatic dynamic assets bundling - removed laravel-mix-glob, used available mix api instead; - removed combining assets; - used node-sass-glob-importer for allowing globbing with sass file, as mix.sass not provide this feature

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,5 @@
 const mix = require( 'laravel-mix' );
-const MixGlob = require( 'laravel-mix-glob' );
-const del = require( 'del' );
+const sassGlobImporter = require('node-sass-glob-importer');
 
 /*
  |--------------------------------------------------------------------------
@@ -24,27 +23,22 @@ const del = require( 'del' );
  * -> public/vendor.css
  */
 
-const mixGlob = new MixGlob( { mix } );
-
-mixGlob
-    .js( 'resources/views/{sections,components}/**/*.js', 'public/js/combine' )
-    .sass( 'resources/views/{sections,components}/**/*.scss', 'public/css/combine' );
-
 mix
     .browserSync( 'localhost:8000' )
     .disableNotifications()
     .extract( 'public/js/vendor.js' )
-    .js( 'resources/js/app.js', 'public/js' )
+    .js( 'resources/views/{sections,components}/**/*.js', 'public/js/app.js' )
+    .js( 'resources/js/app.js', 'public/js/app.js' )
     .postCss( 'resources/css/app.css', 'public/css' )
     .postCss( 'resources/css/vendor.css', 'public/css', [
         require( 'tailwindcss' ),
     ] )
-    .sass( 'resources/sass/app.scss', 'public/css/combine' )
+    .sass( 'resources/sass/app.scss', 'public/css/app.css', {
+        sassOptions: {
+            importer: sassGlobImporter(),
+        }
+    } )
     .sass( 'resources/sass/vendor.scss', 'public/css' )
-    // combine app.css with {components,sections}/styles.css
-    .styles( 'public/css/combine/*', 'public/css/app.css' )
-    // combine app.js with with {components,sections}/script.index
-    .scripts( ['public/js/app.js', 'public/js/combine/*'], 'public/js/app.js' )
     .then( () => {
 
         if (mix.inProduction()) {
