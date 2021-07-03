c699d99d2897c8ab5c074d20c008f75b780fea62
extendslcc <lucas.lcc@hotmail.com>
2021-07-02 22:43:11 -0300

    FEAT( pages/home ): adicionado contexto do projeto a pagina home

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -4,22 +4,39 @@
 @section( 'title', 'Home' )
 
 @section( 'content' )
-   
-   {{-- <div class="container">
+    
+    <div class="container my-5">
         <!--Section: Content-->
-        <section>
+        <section class="col-12">
             <!-- Jumbotron -->
-            <div id="intro" class="p-5 text-center bg-light shadow-5 rounded mb-5">
-                <h1 class="mb-3 h2">Learn Bootstrap 5 with MDB</h1>
-                <p class="mb-3">Best & free guide of responsive web design</p>
-                <a class="btn btn-primary m-2" href="https://www.youtube.com/watch?v=c9B4TPnak1A" role="button" rel="nofollow"
-                   target="_blank">Start tutorial</a>
-                <a class="btn btn-primary m-2" href="https://mdbootstrap.com/docs/standard/" target="_blank"
-                   role="button">Download MDB UI KIT</a>
+            <div id="intro" class="p-4 text-center bg-secondary shadow-5 rounded mb-5">
+                <h1 class="mb-3 h2 text-white">TecWeb 3 - TADS</h1>
+                <div class="mb-3 text-white">
+                    <span>Frameworks:</span>
+                    <div class="flex justify-center">
+                        <ul class="ml-10 list-disc text-justify">
+                            <li><a href="https://laravel.com/">Laravel</a></li>
+                            <li><a href="https://mdbootstrap.com/">MDBootstrap</a></li>
+                            <li><a href="https://tailwindcss.com/">Tailwindcss</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <hr>
+                <p class="mt-3 mb-2 text-sm">
+                    Projeto Desenvolvido para disciplina TecWeb3 para aprendizado do framework laravel.
+                    <br>
+                    ~ Lucas LCC
+                </p>
+                <a target="_blank" href="https://github.com/extendsLcc/TecWeb3_Controle-de-Series"
+                   class="btn btn-dark text-white" rel="nofollow"
+                >
+                    <i class="fab fa-github"></i>
+                    <span>Github</span>
+                </a>
             </div>
             <!-- Jumbotron -->
         </section>
         <!--Section: Content-->
-    </div>--}}
+    </div>
 
 @endsection
