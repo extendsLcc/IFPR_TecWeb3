bafb36b139f0620b68ee839b99a82e44bef62395
extendslcc <lucas.lcc@hotmail.com>
2021-09-10 21:38:19 -0300

    :recycle: REFACTOR( layout/main ): remove crsf token from default layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,22 +3,21 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
     {{-- TODO: change site name by aplication type --}}
     <title>TecWeb 3 - @yield('title')</title>
-    
+
     <meta name="description" content="@yield('meta_description', 'controle series')">
     <meta name="author" content="extendsLcc">
-    
+
     @yield('meta')
-    
+
     <!-- Before-Styles -->
     @stack('before-styles')
     <!-- /Before-Styles -->
-    
+
     <!-- Styles -->
     <link href="{{ asset('css/vendor.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
@@ -30,7 +29,7 @@
     <!-- Body -->
     @yield('body')
     <!-- /Body -->
-    
+
     @stack('before-scripts')
     <!-- Scripts -->
     <script src="{{ asset('js/manifest.js') }}"></script>
