23ff976d499651ef702f251c39ac76de86d22010
extendslcc <lucas.lcc@hotmail.com>
2021-09-24 23:32:05 -0300

    :construction_worker: CLI: adds shell script for installing project - run initial laravel sail installation - add alias to sail to bash

---
+++ install.sh
@@ -0,0 +1,15 @@
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd)":/var/www/html \
+    -w /var/www/html \
+    laravelsail/php80-composer:latest \
+    composer install --ignore-platform-reqs
+
+sailAlias="alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'"
+if grep -Fxq "${sailAlias}" ~/.bashrc
+then
+    echo "Sail already has alias"
+else
+    echo "${sailAlias}" >> ~/.bashrc
+    echo "Sail has been aliased in ~/bashrc"
+fi
