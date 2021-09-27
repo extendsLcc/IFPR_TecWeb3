b92667643196d461a72688022b3399b07491a518
extendslcc <lucas.lcc@hotmail.com>
2021-09-27 00:15:06 -0300

    :star: feat: implements base products listing

--- resources/views/pages/produtos/index/view.blade.php
+++ resources/views/pages/produtos/index/view.blade.php
@@ -5,6 +5,56 @@
 
 @section( 'content' )
 
-    <h1 class="text-white">Página Produtos.INDEX</h1>
+    <div class="container">
+        <div class="row mt-5 px-5">
+            <div class="col-12">
+                <div class="card border-default bg-primary">
+                    <div class="card-header border-color-default d-flex justify-content-between align-items-center">
+                        <span>Produtos - Listagem</span>
+                        <div class="d-flex">
+                            <div class="btn btn-dark">
+                                <span>Cadastrar</span>
+                                <i class="fas fa-plus ms-2"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body text-white">
+
+                        <table class="table table-striped table-hover">
+
+                            <thead>
+                                <tr class="text-white">
+                                    <th>Produto</th>
+                                    <th>Descrição</th>
+                                    <th>Preço</th>
+                                    <th>Estoque</th>
+                                    <th>Ação</th>
+                                </tr>
+                            </thead>
+
+                            @foreach($products as $product)
+                                <tr class="text-white">
+                                    <td>{{ $product->name }}</td>
+                                    <td>{{ $product->description }}</td>
+                                    <td>{{ $product->price }}</td>
+                                    <td>{{ $product->stock }}</td>
+                                    <td>
+                                        <button class="btn btn-dark btn-sm px-2">
+                                            <i class="fas fa-eye fa-lg"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                        </table>
+
+
+                        {{ $products->links() }}
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
